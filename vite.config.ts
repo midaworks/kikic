abe976cd5ea7639bd58353b3aa5d972a901a9f16
Suin Kim <contact@suinkim.me>
2024-12-05 09:58:54 +0900

    chore: install preitter, run lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
     v3_singleFetch: true;
   }
 }
 
 const isStorybook =
-  process.argv[1]?.includes("storybook") &&
-  process.argv[1]?.includes("node_modules");
+  process.argv[1]?.includes('storybook') &&
+  process.argv[1]?.includes('node_modules');
 
 export default defineConfig({
   plugins: [
