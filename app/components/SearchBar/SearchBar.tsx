5ac992ea8074951fc963860c59d9b7d043f433f3
susung1107 <susung1107@gmail.com>
2024-12-26 17:13:36 +0900

    fix: 리뷰 반영

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -1,12 +1,21 @@
-import searchIcon from '../../assets/icons/ic-search.svg';
+// assets
+import SearchIcon from 'public/icons/ic-search.svg';
 
-// searchbar type = main, sub
-// 키워드조회 텍스트 변경 가능
+// components
+import ImageButton from '../ImageButton/ImageButton';
 
-type SearchBarProps = React.InputHTMLAttributes<HTMLInputElement> & {
-  type: 'primary' | 'sub';
+export enum SearchBarVariant {
+  PRIMARY = 'primary',
+  SUB = 'sub',
+}
+
+type SearchBarProps = {
+  variant: SearchBarVariant;
   title: string;
-  onPressSearch: () => void;
+  value: string;
+  placeholder?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: () => void;
 };
 
 const SEARCH_BAR_STYLE = {
@@ -20,34 +29,30 @@ const SEARCH_BAR_STYLE = {
 } as const;
 
 function SearchBar({
-  type = 'primary',
+  variant = SearchBarVariant.PRIMARY,
   title,
-  onPressSearch,
-  ...props
+  value,
+  placeholder,
+  onChange,
+  onSubmit,
 }: SearchBarProps) {
-  const { value, onChange, placeholder } = props;
-
   return (
     <div
-      className={`${SEARCH_BAR_STYLE[type].searchBar} flex items-center justify-center pl-[32px] pr-[26px] py-[20px]`}
+      className={`${SEARCH_BAR_STYLE[variant].searchBar} flex items-center justify-center pl-[32px] pr-[26px] py-[20px] relative`}
     >
-      <p className="text-regular-b text-primary-200 truncate">{title}</p>
-      <div className="w-[1px] h-[18px] bg-gray-500 mx-[19px]" />
+      <p className="text-regular-b text-primary-200 truncate relative pr-[19px] after:content-[''] after:absolute after:top-1/2 after:-translate-y-1/2 after:right-0 after:w-[1px] after:h-[18px] after:bg-gray-500">
+        {title}
+      </p>
+      <div className="w-[19px]" />
       <input
         type="text"
         className="flex-1 focus:outline-none text-content-m bg-transparent"
         value={value}
         onChange={onChange}
-        placeholder={placeholder}
+        placeholder={placeholder || ''}
       />
-      <div className="pl-[26px] flex items-center justify-center">
-        <button
-          className="w-[25px] h-[25px]"
-          type="button"
-          onClick={onPressSearch}
-        >
-          <img src={searchIcon} alt="검색" title="검색" />
-        </button>
+      <div className="pl-[26px]  flex items-center justify-center">
+        <ImageButton src={SearchIcon} onClick={onSubmit} label="검색버튼" />
       </div>
     </div>
   );
--- app/stories/SearchBar.stories.ts
+++ app/stories/SearchBar.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-// import { fn } from '@storybook/test';
+import { fn } from '@storybook/test';
 
-import SearchBar from '~/components/SearchBar/SearchBar';
+import SearchBar, { SearchBarVariant } from '~/components/SearchBar/SearchBar';
 
 const meta = {
   title: 'Components/SearchBar',
@@ -10,6 +10,34 @@ const meta = {
     layout: 'centered',
   },
   tags: ['autodocs'],
+  argTypes: {
+    variant: {
+      description: '검색바 타입',
+      control: 'select',
+      options: ['primary', 'sub'],
+      defaultValue: 'primary',
+    },
+    title: {
+      description: '검색바 제목',
+      control: 'text',
+    },
+    value: {
+      description: '검색어 값',
+      control: 'text',
+    },
+    placeholder: {
+      description: '플레이스홀더 텍스트',
+      control: 'text',
+    },
+    onChange: {
+      description: '입력값 변경 핸들러',
+      action: 'changed',
+    },
+    onSubmit: {
+      description: '검색 제출 핸들러',
+      action: 'submitted',
+    },
+  },
 } satisfies Meta<typeof SearchBar>;
 
 export default meta;
@@ -17,18 +45,22 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
   args: {
-    type: 'primary',
+    variant: SearchBarVariant.PRIMARY,
     title: '키워드조회',
-    onPressSearch: () => {},
+    value: '',
+    onChange: fn(),
+    onSubmit: fn(),
     placeholder: '혹시몰라서 넣어봤어요~',
   },
 };
 
 export const Sub: Story = {
   args: {
-    type: 'sub',
+    variant: SearchBarVariant.SUB,
     title: '마케팅칼럼',
-    onPressSearch: () => {},
+    value: '',
+    onSubmit: fn(),
+    onChange: fn(),
     placeholder: '검색어를 입력해주세요.',
   },
 };
